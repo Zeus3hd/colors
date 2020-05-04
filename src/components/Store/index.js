7f88637d138cb6f3d120ff09665ddde05f9d1acb
Ahd <dafuq.ahd@gmail.com>
2020-05-04 19:01:48 +0300

    add change store category functionality by clicking the category button and display products holding the corresponding category

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -13,33 +13,55 @@ import {
 
 const Store = () => {
   const [data, setData] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("abaya");
   useEffect(() => {
     fetch("/products")
       .then((res) => {
         return res.json();
       })
-      .then((snapshot) => setData(snapshot));
+      .then((snapshot) => {
+        setData(snapshot);
+        console.log(snapshot[0].type["en-US"]);
+      });
   }, []);
+
   return (
     <Wrapper>
       <CategoriesContainer>
-        <CategoryButton active>DRESSES</CategoryButton>
-        <CategoryButton>COATS</CategoryButton>
-        <CategoryButton>ABAYA</CategoryButton>
+        <CategoryButton
+          active={activeCategory === "dress"}
+          onClick={() => setActiveCategory("dress")}
+        >
+          DRESSES
+        </CategoryButton>
+        <CategoryButton
+          active={activeCategory === "coat"}
+          onClick={() => setActiveCategory("coat")}
+        >
+          COATS
+        </CategoryButton>
+        <CategoryButton
+          active={activeCategory === "abaya"}
+          onClick={() => setActiveCategory("abaya")}
+        >
+          ABAYA
+        </CategoryButton>
       </CategoriesContainer>
       <Gallery>
         {data.map((item, i) => {
-          return (
-            <Product key={i} bg={item.imageurl["en-US"]}>
-              <ProductTitle>TITLE HERE</ProductTitle>
-              <ProductPrice>$85,43</ProductPrice>
-              <ProductSellingPointContainer>
-                <ProductSellingPoint>SELLING POINT</ProductSellingPoint>
-                <ProductSellingPoint>SELLING POINT</ProductSellingPoint>
-                <ProductSellingPoint>SELLING POINT</ProductSellingPoint>
-              </ProductSellingPointContainer>
-            </Product>
-          );
+          if (item.type["en-US"] === activeCategory) {
+            return (
+              <Product key={i} bg={item.imageurl["en-US"]}>
+                <ProductTitle>TITLE HERE</ProductTitle>
+                <ProductPrice>$85,43</ProductPrice>
+                <ProductSellingPointContainer>
+                  <ProductSellingPoint>SELLING POINT</ProductSellingPoint>
+                  <ProductSellingPoint>SELLING POINT</ProductSellingPoint>
+                  <ProductSellingPoint>SELLING POINT</ProductSellingPoint>
+                </ProductSellingPointContainer>
+              </Product>
+            );
+          }
         })}
       </Gallery>
     </Wrapper>
